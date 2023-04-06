23239bb89ce16fdbe0da017450d30f7c1855eec3
captain-mike <info@grafikweb.it>
2023-04-06 18:04:18 +0200

    esempi

--- m1/s2/g2/tastierino/js/script.js
+++ m1/s2/g2/tastierino/js/script.js
@@ -3,7 +3,13 @@ function addNumber(n){
     
     let display = document.getElementById('display');
 
-    console.dir(n)
+    
     display.value += n;
 
+}
+
+function total(){
+    let display = document.getElementById("display");
+    let result = eval(display.value.replaceAll('x','*'));
+    display.value = result;
 }
