af3fd19e40645147149db9f71f7937225d925310
captain-mike <info@grafikweb.it>
2023-05-31 13:00:51 +0200

    esempio in classe

--- m3/s2/g3/crud/src/app/app.module.ts
+++ m3/s2/g3/crud/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu/menu.component';
+import { AddPizzaComponent } from './menu/add-pizza/add-pizza.component';
+import { PizzaComponent } from './menu/pizza/pizza.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AddPizzaComponent,
+    PizzaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
