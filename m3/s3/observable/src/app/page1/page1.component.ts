ff7c46169571876ea6d8f281fb6b81b770aee13d
captain-mike <info@grafikweb.it>
2023-06-05 15:50:36 +0200

    commenti

--- m3/s3/observable/src/app/page1/page1.component.ts
+++ m3/s3/observable/src/app/page1/page1.component.ts
@@ -10,45 +10,43 @@ export class Page1Component implements OnInit, OnDestroy{
 
 
   sub!:Subscription;//oggetto che sottoscriverà l'observable
-  conteggio!:number;
+  conteggio!:number;//il valore dell'observable (observer)
 
   ngOnInit(): void {
 
     const intervallo = new Observable(observer => {
       let count = 0;
       setInterval(()=>{
-
-        observer.next(count)//emette il valore che si trova tra le parentesi
-
-        if(count == 5){
-          observer.complete();//completo l'observable
-          console.log('completo');
+        observer.next(count);//next emette il valore che si trova tra le parentesi
+        if(count === 5){
+          observer.complete();//completo l'observable se valore == 5
+          console.log('Observer Completato');
         }
-        if(count > 3){
-          observer.error(new Error('Count troppo grande'));
+        if(count > 3){//definisco un errore
+          observer.error(new Error('Count è troppo grande'));//errore ma l'observable prosegue
         }
-
-
         count++;
       },1000)
+    });
+
+
+    this.sub = intervallo.pipe(map((conta) => {//mappo l'observable
+      this.conteggio = Number(conta)//salvo il dato proveniente dall'observable
+      return `Siamo al numero ${conta}`;//modifico il dato prima dell'output
+    }))
+    .subscribe({
+      next: (numero) => {
+        console.log(numero)
+      },
+      error: (err) => console.error(err),//senza questo l'errore è bloccante
+      complete: () => console.log('complete')
 
     })
 
-
-    this.sub = intervallo
-    .pipe(map( n => {
-      this.conteggio = Number(n);
-      return `Siamo al numero ${n}`;
-    }))
-    .subscribe({
-      next: (numero)=> console.log(numero),
-      error: (err)=> console.error(err),
-      complete: ()=>console.log('completo')
-    });
-
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
+  ngOnDestroy(): void {//se il componente viene distrutto
+    this.sub.unsubscribe();//termino l'iscrizione
+    console.log('Observable scaricato');
   }
 }
