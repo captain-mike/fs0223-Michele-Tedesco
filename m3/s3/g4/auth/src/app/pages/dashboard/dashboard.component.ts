54d34cacc80aa163130974e82fef1b55b161f140
captain-mike <info@grafikweb.it>
2023-06-09 18:33:19 +0200

    crud

--- m3/s3/g4/auth/src/app/pages/dashboard/dashboard.component.ts
+++ m3/s3/g4/auth/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Ipizza } from './ipizza';
+import { PizzaService } from './pizza.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+  pizze:Ipizza[] = [];
+
+  newPizza:Partial<Ipizza> = {
+    gusto:'',
+    prezzo:0
+  }
+
+  constructor(private pizzaSvc:PizzaService){}
+
+  ngOnInit(){
+    this.pizzaSvc.getAll().subscribe((pizze)=>{
+      this.pizze = pizze;
+    })
+  }
+
+  create(){
+    this.pizzaSvc.post(this.newPizza)
+    .subscribe(data=>{
+      this.pizze.push(data)
+    })
+  }
+
+  delete(id:number){
+    this.pizzaSvc.delete(id)
+    .subscribe(data=>{
+      let index = this.pizze.findIndex(p => p.id == id)
+      this.pizze.splice(index,1)
+    })
+  }
 
 }
--- m3/s3/g4/auth/src/environments/environment.development.ts
+++ m3/s3/g4/auth/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://localhost:3000',
+  apiPizze: 'http://localhost:3000/pizze'
 };
